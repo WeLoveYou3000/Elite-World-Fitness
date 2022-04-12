dfe067a2447e66dafdc8db8021ade4599ff04a5e
Gaurav Ganesh Hegde <53887647+gauravvvmehhh@users.noreply.github.com>
2022-04-12 14:09:04 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,89 +1,345 @@
-<?php 
-
-include 'config.php';
-
-error_reporting(0); // For not showing any error
-
-if (isset($_POST['submit'])) { // Check press or not Post Comment Button
-	$name = $_POST['name']; // Get Name from form
-	$email = $_POST['email']; // Get Email from form
-	$experience = $_POST['experience']; //Get Experience
-	$comments = $_POST['comments']; // Get Comment from form
-	
-
-	$sql = "INSERT INTO review (name, email, experience, comments)
-			VALUES ('$name', '$email','$experience', '$comments')";
-	$result = mysqli_query($conn, $sql);
-	if ($result) {
-		echo "<script>alert('Comment added successfully.')</script>";
-	} else {
-		echo "<script>alert('Comment does not add.')</script>";
-	}
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<link rel="stylesheet" type="text/css" href="style.css">
-
-	<title>Review Page</title>
+	<title> ELITE WORLD FITNESS
+          
+	</title>
 </head>
 <body>
-	    <form  action="../Gymwebsite/index.php" method="get">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">HOME</button>
-    </form>&nbsp&nbsp
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,300;1,300&display=swap" rel="stylesheet">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
+<?php include'menu.php';?>
 
-	<div class="wrapper">
-		<form action="" method="POST" class="form">
-			<input type="radio" name="experience" value="bad"/> Bad &nbsp
-                <input type="radio" name="experience" value="average"/> Average &nbsp
-                <input type="radio" name="experience" value="good"/> Good
-			<div class="row">
-				<div class="input-group">
-					<label for="name">Name</label>
-					<input type="text" name="name" id="name" placeholder="Enter your Name" required>
-				</div>
-				<div class="input-group">
-					<label for="email">Email</label>
-					<input type="email" name="email" id="email" placeholder="Enter your Email" required>
-				</div>
-			</div>
-			<div class="input-group textarea">
-				<label for="comment">Comment</label>
-				<textarea id="comment" name="comments" placeholder="Enter your Comment" required></textarea>
-			</div>
-			<div class="input-group">
-				<button name="submit" class="btn">Post Comment</button>
-			</div>
-		</form>
-		<div class="prev-comments">
-			<?php 
-			
-			$sql = "SELECT * FROM review";
-			$result = mysqli_query($conn, $sql);
-			if (mysqli_num_rows($result) > 0) {
-				while ($row = mysqli_fetch_assoc($result)) {
+  
+  <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Images/Fitness1.jpg" alt="Get Your Fitness Plan Today" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3><I>"Fitness isn’t a seasonal hobby, It’s a healthy lifestyle" <br/>So Get Your Workout Plan Today.</h3>
+        <form method="get" action="contact.php">
+        <button type="submit" class="btn btn-success">Register Now</button>
+        </form>
 
-			?>
-			<div class="single-item">
-				<h4><?php echo $row['name']; ?></h4>
-				<a href="mailto:<?php echo $row['email']; ?>"><?php echo $row['email']; ?></a>
-				<h5><?php echo $row['experience']; ?></h5>
-				<p><?php echo $row['comments']; ?></p>
-			</div>
-			<?php
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="Images/g18.jpg" alt="Get Your Fitness Plan Today" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3><i>"Some people want it to happen, some wish it would happen, others make it happen"<br/> Get Your Fitness Plan To Make a Change</h3>
+        <form method="get" action="contact.php">
+        <button type="submit" class="btn btn-success">Register Now</button>
+        </form>
 
-				}
-			}
-			
-			?>
-		</div>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="Images/g20.jpg" alt="Get Your Fitness Plan Today" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3><i>"Success is what comes after your stop making excuses"<br/>"Nothing will work unless you do"<br/>Get Your Membership And Start Your Success Story Today</h3>
+        <form method="get" action="contact.php">
+        <button type="submit" class="btn btn-success">Register Now</button>
+        </form>
+
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center" style="font-size: 50px;
+                                          background: -webkit-linear-gradient(#f00, #fcc);
+                                           -webkit-background-clip: text;
+                                            -webkit-text-fill-color: transparent;
+                        "><b>About Us</h2>
 	</div>
+  <div class="container-fluid"> 
+	 <div class="row" >
+		    <div class="col-lg-6 col-md-6 col-12">
+			       <img src="Images/about.jpg" class="img-fluid aboutimg">
+        </div>
+         <div class="col-lg-6 col-md-6 col-12">
+             <h2 class="display-4"><B>STORY ABOUT US<B></h2>
+             <p class="py-5">Created by personal trainers, ELITEWORLD — a group of friends team passionate about helping you achieve your health and fitness goals.
+
+             With hundreds of professional workouts, healthy recipes and informative articles, you’ll have everything you need to reach your personal fitness goals !!!</p>
+             <a href="about.php" class="btn btn-success">Check More Of Us</a>
+
+          </div>
+    </div>
+  </div>
+</section>
+<div class="about-counter" style="background-color:#4158D0; background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="about-counter-text" >
+                        <div class="single-counter">
+                            <h1  class="text-center">98</h1>
+                            <p class="text-center">Programs</p>
+                        </div>
+                        <div class="single-counter">
+                            <h1 class="text-center">14</h1>
+                            <p class="text-center">Locations</p>
+                        </div>
+                        <div class="single-counter">
+                            <h1 class="text-center">21k+</h1>
+                            <p class="text-center">Members</p>
+                        </div>
+                        <div class="single-counter">
+                            <h1 class="text-center">34</h1>
+                            <p class="text-center">Coaches</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<section class="my-5">
+  <div class="container-fluid"> 
+   <div class="row" >
+        <div class="col-lg-6 col-md-6 col-12">
+             <img src="Images/award.jpg" class="img-fluid aboutimg">
+        </div>
+         <div class="col-lg-6 col-md-6 col-12">
+             <h2 class="display-4"><B>BEST GYM AWARD<B></h2>
+             <p class="py-5">We are happy to share this news with our members that we are awarded as the best fitness online portal. 
+                            This has become possible with the love and support shown by you people. We pledge towards providing more support
+                            and better services.
+                          With hundreds of professional workouts, healthy recipes and informative articles, you’ll have everything you need to reach your personal fitness goals !!!</p>
+
+          </div>
+    </div>
+  </div>
+</section>
+<section>
+  <div class="py-5">
+    <h2 class="text-center" style="font-size: 50px;
+                                          background: -webkit-linear-gradient(45deg,#3ee, #333);
+                                           -webkit-background-clip: text;
+                                            -webkit-text-fill-color: transparent;
+                        "><b>Our Services</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+     <div class="col-lg-4 col-md-4 col-12">
+       <div class="card">
+          <img class="card-img-top" src="Images/crossfit.jpg" alt="Card image" width="300" height="400">
+            <div class="card-body">
+              <h4 class="card-title">CrossFit</h4>
+                <p class="card-text">Burn That Layer Of Stubborn Fat</p>
+              </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+       <div class="card">
+          <img class="card-img-top" src="Images/yoga.jpg" alt="Card image" width="300" height="400">
+            <div class="card-body">
+              <h4 class="card-title">Yoga</h4>
+                <p class="card-text">Meditate And Find Inner Peace With Us</p>
+              </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+       <div class="card">
+          <img class="card-img-top" src="Images/gym.jpg" alt="Card image" width="300" height="400">
+            <div class="card-body">
+              <h4 class="card-title">GYM</h4>
+                <p class="card-text">Get Those Huge Muscle Gains With Us</p>
+              </div>
+        </div>
+      </div>
+      
+      <div class="col-lg-4 col-md-4 col-12">
+       <div class="card">
+          <img class="card-img-top" src="Images/zumba.jpg" alt="Card image" width="300" height="400">
+            <div class="card-body">
+              <h4 class="card-title">Zumba</h4>
+                <p class="card-text">Come Dance With Us And Burn Those Calories</p>
+              </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+       <div class="card">
+          <img class="card-img-top" src="Images/cardio.jpg" alt="Card image" width="300" height="400">
+            <div class="card-body">
+              <h4 class="card-title">Cardio</h4>
+                <p class="card-text">Run For An Good Life Of Yours</p>
+              </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+       <div class="card">
+          <img class="card-img-top" src="Images/kickbox.jpg" alt="Card image" width="300" height="400">
+            <div class="card-body">
+              <h4 class="card-title">Kick Boxing</h4>
+                <p class="card-text">Punch And Kick Like An MMA Star</p>
+              </div>
+        </div>
+      </div>
+   </div>
+  </div>
+</section>
+<section>
+  <div class="py-5">
+    <h2 class="text-center" style="font-size: 50px;
+                                          background: -webkit-linear-gradient(#494964, #6f6f89);
+                                           -webkit-background-clip: text;
+                                            -webkit-text-fill-color: transparent;
+                        "><b>Our GYM</b></h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="Images/g1.jpg" class="img-fluid pb-4" > 
+      </div><div class="col-lg-4 col-md-4 col-12">
+        <img src="Images/g15.jpg" class="img-fluid pb-4" width="400" height="500" > 
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="Images/g9.jpg" class="img-fluid pb-4"> 
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="Images/g8.jpg" class="img-fluid pb-4" > 
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="Images/g10.jpg" class="img-fluid pb-4"> 
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="Images/g7.jpg" class="img-fluid pb-4"> 
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<section class="trainer-section spad" style="background-image: url(Images/banner-bg.jpg);" >
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <br/><h2 class="text-center" style="font-size: 50px;
+                                          background: -webkit-linear-gradient(#eee, #333);
+                                           -webkit-background-clip: text;
+                                            -webkit-text-fill-color: transparent;
+                        "><i><b>EXPERT TRAINERS</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-trainer-item">
+                        <img src="Images/trainer-1.jpg" alt="">
+                        <div class="trainer-text">
+                           <br/> <h5 class="text-white">Sam Rodrigues</h5>
+                            <span class="text-white">Leader</span>
+                            <p class="text-white">Sam Rodrigues Has Worked In The Fitness Industry Since His Early 
+                                College Days And Is a Certified Trainer In KickBoxing. </p> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-trainer-item">
+                        <img src="Images/trainer-2.jpg" alt="">
+                        <div class="trainer-text">
+                           <br/> <h5 class="text-white">Manish Pillai</h5>
+                            <span class="text-white">Gym Coach</span>
+                            <p class="text-white">Manish Pillai Is Not Only a Gym Coach But Also a Yoga Expert With Work 
+                                Experience. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-trainer-item">
+                        <img src="Images/trainer-3.jpg" alt="">
+                        <div class="trainer-text">
+                           <br/> <h5 class="text-white">Shreya Rai</h5>
+                            <span class="text-white">Fitness Trainer</span>
+                            <p class="text-white">Shreya Rai Is And Running and Flexibilty 
+                                Expert And Also Well Known For Zumba Coaching .</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><br/><br/>
+
+
+<footer class="footer-section" style="background-image: url(Images/g13.jpg);">>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="contact-option">
+                        <span class="text-white">Address</span>
+                        <p class="text-white">Fours Square BKC,Bandra(E)</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="contact-option">
+                        <span class="text-white">Phone</span>
+                        <p class="text-white">7738159247 - (0251) 2734608</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="contact-option">
+                        <span class="text-white">Email</span>
+                        <p class="text-white">eliteworldfitness@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-text text-white">
+                <ul>
+                    <li><a href="#">Term&Use</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+                <p>&copy;<p>
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </a></p></p>
+              <div class="row">
+                <div class="column p-3">
+                  <img src="Images/gp.png" alt="Snow" style="width:50%" >
+                  <p>eliteworldfitness.co.in</p>
+                </div>
+                <div class="column p-3">
+                    <img src="Images/tw.png" alt="Forest" style="width:50%">
+                    <p>BeingEliteWorldFitness</p>
+                </div>
+                <div class="column p-3">
+                    <img src="Images/fb.png" alt="Mountains" style="width:50%">
+                    <p>BeingEliteFitness</p>
+                </div>
+                <div class="column p-3">
+                    <img src="Images/insta.png" alt="Mountains" style="width:15%">
+                    <p>BeingElite</p>
+                </div>
+              </div>
+
+
+            </div>
+        </div>
+    </footer>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 </html>
